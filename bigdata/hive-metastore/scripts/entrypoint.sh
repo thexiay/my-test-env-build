04dd7f9fe47add60f36b76e82cce2d8c3b338f40
thexia <thexia@tencent.com>
2023-07-15 17:58:28 +0800

    增加stop时处理15信号

--- bigdata/hive-metastore/scripts/entrypoint.sh
+++ bigdata/hive-metastore/scripts/entrypoint.sh
@@ -13,4 +13,20 @@ if [ $? -eq 1 ]; then
   "${HIVE_HOME}"/bin/schematool -initSchema -dbType mysql
 fi
 
-"${HIVE_HOME}"/bin/start-metastore
+"${HIVE_HOME}"/bin/start-metastore &
+pid=${!}
+
+# trap和常驻处理逻辑
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -15 "$pid"
+    # wait是用来阻塞当前进程的执行，直至指定的子进程执行结束后，才继续执行
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!};term_handler' TERM
+while true 
+do
+  tail -f /dev/null & wait ${!} # 这一段是让本sh不退出，并且trap是等到当前没有任务后才处理，因为是wait状态所以可以直接处理
+done
--- bigdata/minio/entrypoint.sh
+++ bigdata/minio/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
 MINIO_DEFAULT_BUCKETS=${MINIO_DEFAULT_BUCKETS:-"default"}
 echo "@ is" "$@"
 echo "$@"
 /usr/bin/docker-entrypoint.sh "$@" > /dev/stdout &
+pid=${!}
 
 # 为local设置权限
 mc config host add local http://localhost:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD} --api s3v4
@@ -15,5 +15,20 @@ else
   mc mb local/"${MINIO_DEFAULT_BUCKETS}"
   echo "Bucket '${MINIO_DEFAULT_BUCKETS}' created."
 fi
-tail -f /dev/null
+
+# trap和常驻处理逻辑
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -15 "$pid"
+    # wait是用来阻塞当前进程的执行，直至指定的子进程执行结束后，才继续执行
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!};term_handler' TERM
+while true 
+do
+  tail -f /dev/null & wait ${!} # 这一段是让本sh不退出，并且trap是等到当前没有任务后才处理，因为是wait状态所以可以直接处理
+done
+
 
